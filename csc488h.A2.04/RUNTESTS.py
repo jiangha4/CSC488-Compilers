e0ca6b07cf76cf0536f17b1350ba435cc2953161
Timo Vink <timovink@gmail.com>
2015-01-23 23:31:26 -0500

    Test runner now handles both positive and negative test cases.

--- csc488h.A2.04/RUNTESTS.py
+++ csc488h.A2.04/RUNTESTS.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 import subprocess
 import sys
@@ -12,6 +13,70 @@ TEST_DIR = "./test"
 
 
 '''
+Enum describing the possible test result types.
+'''
+class ResultType(Enum):
+    passed = 1
+    failed = 2
+    did_not_run = 3
+
+
+'''
+Class describing the test result type as well as optionally a detail string.
+'''
+class TestResult:
+    def __init__(self, test, result_type, detail=None):
+        self.test_name = test
+        self.type = result_type
+        self.details = detail
+
+
+'''
+Return True iff the parser succesfully parsed the input.
+'''
+def parsing_succeeded(parser_output):
+    return "Exception during Parsing" not in parser_output
+
+
+'''
+Run the given test and return a TestResult.
+'''
+def run_test(path):
+    # Check if this test has a valid name
+    is_pass_test = path.endswith(".pass.488")
+    is_fail_test = path.endswith(".fail.488")
+    if not (is_pass_test or is_fail_test):
+        return TestResult(path, ResultType.did_not_run)
+
+    # Run parser on test
+    args = ["java", "-jar", COMPILER_BIN, path]
+    raw_output = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    parser_output = raw_output.decode("utf-8")
+
+    # Check if this is a positive or negative test case
+    parse_success = parsing_succeeded(parser_output)
+    test_success = parse_success if is_pass_test else not parse_success
+
+    # Return result
+    if test_success:
+        return TestResult(path, ResultType.passed)
+    else:
+        return TestResult(path, ResultType.failed, parser_output)
+
+
+'''
+Prints the result of running a single test.
+'''
+def print_test_result(result):
+    if result.type == ResultType.passed:
+        print("\033[92m.\033[0m", end="")
+    elif result.type == ResultType.failed:
+        print("\033[91mF\033[0m", end="")
+    else:
+        print("\033[93m?\033[0m", end="")
+
+
+'''
 Run all tests in the directory at the given path and print the results.
 '''
 def run_tests(path):
@@ -22,39 +87,38 @@ def run_tests(path):
     tests = [d for d in item_paths if not d in subdirs if d.endswith(".488")]
 
     # Run tests
-    errors = []
+    results = []
     if len(tests):
-        # Print path
-        print("" + path + ":\n  ", end="")
+        # Print directory path
+        print("\033[94m" + path + "\033[0m:\n  ", end="")
     for test_path in tests:
-        # Run parser on test
-        args = ["java", "-jar", COMPILER_BIN, test_path]
-        raw_output = subprocess.check_output(args, stderr=subprocess.STDOUT)
-        parser_output = raw_output.decode("utf-8")
-
-        # Check and print result
-        if "Exception during Parsing" in parser_output:
-            errors.append((test_path, parser_output))
-            print("\033[91mF\033[0m", end="")
-        else:
-            print("\033[92m.\033[0m", end="")
-    print("\n")
+        # Run parser on test, print result
+        result = run_test(test_path)
+        print_test_result(result)
+        results.append(result)
+    if len(tests):
+        print("\n")
 
     # Print detailed error messages
-    for test_path, err_detail in errors:
-        print ("  \033[91m" + test_path + "\033[0m:")
-        indented = "    " + str.join("\n    ", err_detail.split(sep='\n')[1:-2])
+    errors = [r for r in results if r.type == ResultType.failed]
+    for err_result in errors:
+        print ("  \033[91m" + err_result.test_name + "\033[0m:")
+        indented = "    " + str.join("\n    ", err_result.details.split(sep='\n')[1:-2])
         print(indented + "\n")
 
-    # Recursively run tests in subdirectories
-    num_tests = len(tests)
-    num_errors = len(errors)
-    for subdir in subdirs:
-        sub_tests, sub_erros = run_tests(subdir)
-        num_tests += sub_tests
-        num_errors += sub_erros
+    # Print invalid tests
+    did_not_runs = [r for r in results if r.type == ResultType.did_not_run]
+    if did_not_runs:
+        print ("  \033[93mDid not run due to invalid name\033[0m:")
+        for dnr_name in [r.test_name for r in did_not_runs]:
+            print ("    " + dnr_name)
+        print("")
 
-    return num_tests, num_errors
+    # Recursively run tests in subdirectories
+    for subdir in subdirs:
+        results += run_tests(subdir)
+
+    return results
 
 
 '''
@@ -63,11 +127,18 @@ Main.
 if __name__ == '__main__':
     # Run tests, measure performance
     start = timer()
-    num_tests, num_errs = run_tests(TEST_DIR)
+    results = run_tests(TEST_DIR)
     run_time = int(1000 * (timer() - start))
 
     # Print results
-    print("\033[91m" if num_errs else "\033[92m", end="")
-    line1 = "Ran " + str(num_tests) + " tests in " + str(run_time) + "ms."
-    line2 = str(num_tests - num_errs) + " passed, " + str(num_errs) + " failed."
-    print(line1 + " " + line2 + "\033[0m")
+    num_pass = len([r for r in results if r.type == ResultType.passed])
+    num_fail = len([r for r in results if r.type == ResultType.failed])
+    num_dnr = len([r for r in results if r.type == ResultType.did_not_run])
+
+    print("\033[91m" if num_fail else "\033[93m" if num_dnr else "\033[92m", end="")
+    run_time = "Ran " + str(len(results)) + " tests in " + str(run_time) + "ms."
+    pass_str = str(num_pass) + " passed" if num_pass else ""
+    fail_str = str(num_fail) + " failed" if num_fail else ""
+    dnr_str = str(num_dnr) + " didn't run" if num_dnr else ""
+    result_strs = [s for s in [pass_str, fail_str, dnr_str] if s != ""]
+    print(run_time + " " + str.join(", ", result_strs) + ".\033[0m")
