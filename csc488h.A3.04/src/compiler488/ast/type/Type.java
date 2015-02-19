1a09b7e0751d2706173289475caf1f43ac6d33bd
Maria Yancheva <maria.yancheva@gmail.com>
2015-02-19 16:36:34 -0500

    Added implementation of ASTVisitable to type classes

--- csc488h.A3.04/src/compiler488/ast/type/Type.java
+++ csc488h.A3.04/src/compiler488/ast/type/Type.java
@@ -1,9 +1,15 @@
 package compiler488.ast.type;
 
+import compiler488.ast.ASTVisitable;
+import compiler488.ast.ASTVisitor;
 import compiler488.ast.BaseAST;
 
 /**
  * A placeholder for types.
  */
-public abstract class Type extends BaseAST {
+public abstract class Type extends BaseAST implements ASTVisitable {
+	@Override
+	public void accept(ASTVisitor visitor) {
+		visitor.visit(this);
+	}
 }
