5ffb96d62bee4bcbf5d809bbb60ad6c65b587131
Maria Yancheva <maria.yancheva@gmail.com>
2015-02-19 16:34:05 -0500

    Added implementation of ASTVisitable to expression classes

--- csc488h.A3.04/src/compiler488/ast/expn/Expn.java
+++ csc488h.A3.04/src/compiler488/ast/expn/Expn.java
@@ -1,10 +1,16 @@
 package compiler488.ast.expn;
 
+import compiler488.ast.ASTVisitable;
+import compiler488.ast.ASTVisitor;
 import compiler488.ast.BaseAST;
 import compiler488.ast.Printable;
 
 /**
  * A placeholder for all expressions.
  */
-public abstract class Expn extends BaseAST implements Printable {
+public abstract class Expn extends BaseAST implements Printable, ASTVisitable {
+	@Override
+	public void accept(ASTVisitor visitor) {
+		visitor.visit(this);
+	}
 }
