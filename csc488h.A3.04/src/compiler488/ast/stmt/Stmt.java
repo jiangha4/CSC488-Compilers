4e3af0b8ea00f100e2b6c700b1a7e5a28c2f111a
Maria Yancheva <maria.yancheva@gmail.com>
2015-02-19 16:35:28 -0500

    Added implementation of ASTVisitable to statement classes

--- csc488h.A3.04/src/compiler488/ast/stmt/Stmt.java
+++ csc488h.A3.04/src/compiler488/ast/stmt/Stmt.java
@@ -1,9 +1,15 @@
 package compiler488.ast.stmt;
 
+import compiler488.ast.ASTVisitable;
+import compiler488.ast.ASTVisitor;
 import compiler488.ast.BaseAST;
 
 /**
  * A placeholder for statements.
  */
-public abstract class Stmt extends BaseAST {
+public abstract class Stmt extends BaseAST implements ASTVisitable {
+	@Override
+	public void accept(ASTVisitor visitor) {
+		visitor.visit(this);
+	}
 }
