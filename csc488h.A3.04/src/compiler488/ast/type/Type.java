80cafa99eaaefd877421cef91e9b84c2c50f6288
Maria Yancheva <maria.yancheva@gmail.com>
2015-02-20 12:00:11 -0500

    Refactoring AST traversal for types

--- csc488h.A3.04/src/compiler488/ast/type/Type.java
+++ csc488h.A3.04/src/compiler488/ast/type/Type.java
@@ -1,15 +1,9 @@
 package compiler488.ast.type;
 
-import compiler488.ast.ASTVisitable;
-import compiler488.ast.ASTVisitor;
 import compiler488.ast.BaseAST;
 
 /**
  * A placeholder for types.
  */
-public abstract class Type extends BaseAST implements ASTVisitable {
-	@Override
-	public void accept(ASTVisitor visitor) {
-		visitor.visit(this);
-	}
+public abstract class Type extends BaseAST {
 }
