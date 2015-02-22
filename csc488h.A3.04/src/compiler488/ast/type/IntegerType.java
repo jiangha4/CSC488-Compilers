a1dc839bde1eea24c95916d62868546892578312
yancheva <maria.yancheva@gmail.com>
2015-02-22 13:37:35 -0500

    Added helper methods to convert Type to SymbolType

--- csc488h.A3.04/src/compiler488/ast/type/IntegerType.java
+++ csc488h.A3.04/src/compiler488/ast/type/IntegerType.java
@@ -1,6 +1,7 @@
 package compiler488.ast.type;
 
 import compiler488.ast.ASTVisitor;
+import compiler488.symbol.SymbolTable.SymbolType;
 
 
 /**
@@ -9,6 +10,10 @@ import compiler488.ast.ASTVisitor;
 public class IntegerType extends Type {
     public String toString() {
         return "integer";
+    }
+    
+    public SymbolType toSymbolType() {
+    	return SymbolType.INTEGER;
     }
     
     @Override
