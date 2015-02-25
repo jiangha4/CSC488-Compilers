967edf4077a9719cc4ed2fd055eebfe2cd3ad536
Timo Vink <timovink@gmail.com>
2015-02-25 17:34:44 -0500

    Clean up RoutineDecl.

--- csc488h.A3.04/src/compiler488/ast/decl/RoutineDecl.java
+++ csc488h.A3.04/src/compiler488/ast/decl/RoutineDecl.java
@@ -11,103 +11,110 @@ import compiler488.ast.type.Type;
  * Represents the declaration of a function or procedure.
  */
 public class RoutineDecl extends Declaration {
-    /** The formal parameters for this routine (if any.)
-     *
-     * <p>This value must be non-<code>null</code>. If absent, use an empty
-     * list instead.</p>
-     */
-    private ASTList<ScalarDecl> parameters =  new ASTList<ScalarDecl>();
+	/** The formal parameters for this routine (if any.)
+	*
+	* <p>This value must be non-<code>null</code>. If absent, use an empty
+	* list instead.</p>
+	*/
+   private ASTList<ScalarDecl> parameters =  new ASTList<ScalarDecl>();
 
-    /** The body of this routine (if any.) */
-    private Scope body = null;
+   /** The body of this routine (if any.) */
+   private Scope body = null;
 
-    /**
-     * Construct a function with parameters, and a definition of the body.
-     *   @param  name	      Name of the routine
-     *   @param  type	      Type returned by the function
-     *   @param  parameters   List of parameters to the routine
-     *   @param  body	      Body scope for the routine
-     */
-    public RoutineDecl(String name, Type type, ASTList<ScalarDecl> parameters, Scope body, SourceCoord sourceCoord) {
-        super(name, type, sourceCoord);
+	/**
+	 * Construct a function with parameters, and a definition of the body.
+	 *   @param  name		  Name of the routine
+	 *   @param  type		  Type returned by the function
+	 *   @param  parameters   List of parameters to the routine
+	 *   @param  body		  Body scope for the routine
+	 */
+	public RoutineDecl(String name, Type type, ASTList<ScalarDecl> parameters, Scope body, SourceCoord sourceCoord) {
+		super(name, type, sourceCoord);
 
-        parameters.setParentNode(this);
-        this.parameters = parameters;
-        body.setParentNode(this);
-        this.body = body;
-    }
+		parameters.setParentNode(this);
+		this.parameters = parameters;
+		body.setParentNode(this);
+		this.body = body;
+	}
 
-    /**
-     * Construct a function with no parameters, and a definition of the body.
-     *   @param  name	      Name of the routine
-     *   @param  type	      Type returned by the function
-     *   @param  body	      Body scope for the routine
-     */
-    public RoutineDecl(String name, Type type, Scope body, SourceCoord sourceCoord) {
-        this(name, type, new ASTList<ScalarDecl>(), body, sourceCoord);
-    }
+	/**
+	 * Construct a function with no parameters, and a definition of the body.
+	 *   @param  name		  Name of the routine
+	 *   @param  type		  Type returned by the function
+	 *   @param  body		  Body scope for the routine
+	 */
+	public RoutineDecl(String name, Type type, Scope body, SourceCoord sourceCoord) {
+		this(name, type, new ASTList<ScalarDecl>(), body, sourceCoord);
+	}
 
-    /**
-     * Construct a procedure with parameters, and a definition of the body.
-     *   @param  name	      Name of the routine
-     *   @param  parameters   List of parameters to the routine
-     *   @param  body	      Body scope for the routine
-     */
-    public RoutineDecl(String name, ASTList<ScalarDecl> parameters, Scope body, SourceCoord sourceCoord) {
-        this(name, null, parameters, body, sourceCoord);
+	/**
+	 * Construct a procedure with parameters, and a definition of the body.
+	 *   @param  name		  Name of the routine
+	 *   @param  parameters   List of parameters to the routine
+	 *   @param  body		  Body scope for the routine
+	 */
+	public RoutineDecl(String name, ASTList<ScalarDecl> parameters, Scope body, SourceCoord sourceCoord) {
+		this(name, null, parameters, body, sourceCoord);
 
-        this.parameters = parameters;
-        this.body = body;
-    }
+		this.parameters = parameters;
+		this.body = body;
+	}
 
-    /**
-     * Construct a procedure with no parameters, and a definition of the body.
-     *   @param  name	      Name of the routine
-     *   @param  body	      Body scope for the routine
-     */
-    public RoutineDecl(String name, Scope body, SourceCoord sourceCoord) {
-        this(name, null, new ASTList<ScalarDecl>(), body, sourceCoord);
-    }
+	/**
+	 * Construct a procedure with no parameters, and a definition of the body.
+	 *   @param  name		  Name of the routine
+	 *   @param  body		  Body scope for the routine
+	 */
+	public RoutineDecl(String name, Scope body, SourceCoord sourceCoord) {
+		this(name, null, new ASTList<ScalarDecl>(), body, sourceCoord);
+	}
 
-    public ASTList<ScalarDecl> getParameters() {
-        return parameters;
-    }
+	public ASTList<ScalarDecl> getParameters() {
+		return parameters;
+	}
 
-    public Scope getBody() {
-        return body;
-    }
+	public Scope getBody() {
+		return body;
+	}
 
-    @Override
-    public void prettyPrint(PrettyPrinter p) {
-        if (type == null) {
-            p.print("procedure ");
-        } else {
-            type.prettyPrint(p);
-            p.print(" function ");
-        }
+	public boolean isFunctionDecl() {
+		return this.type != null;
+	}
 
-        p.print(name);
+    public boolean isProcedureDecl() {
+		return !isFunctionDecl();
+	}
 
-        if (parameters.size() > 0) {
-            p.print("(");
-            parameters.prettyPrintCommas(p);
-            p.print(")");
-        }
+	@Override
+	public void prettyPrint(PrettyPrinter p) {
+		if (type == null) {
+			p.print("procedure ");
+		} else {
+			type.prettyPrint(p);
+			p.print(" function ");
+		}
 
-        if (body != null) {
-            p.print(" ");
-            body.prettyPrint(p);
-        }
-    }
-    
-    @Override
+		p.print(name);
+
+		if (parameters.size() > 0) {
+			p.print("(");
+			parameters.prettyPrintCommas(p);
+			p.print(")");
+		}
+
+		if (body != null) {
+			p.print(" ");
+			body.prettyPrint(p);
+		}
+	}
+
+	@Override
 	public void accept(ASTVisitor visitor) {
-    	
-    	// S11/S12: Declare function with/without parameters and with specified type
-    	// S17/S18: Declare procedure with/without parameters
-    	// S04/S08: Start function/procedure scope
+		// S11/S12: Declare function with/without parameters and with specified type
+		// S17/S18: Declare procedure with/without parameters
+		// S04/S08: Start function/procedure scope
 		visitor.visit(this);
-		
+
 		if (type != null) {
 			type.accept(visitor);
 		}
@@ -117,7 +124,7 @@ public class RoutineDecl extends Declaration {
 		if (body != null) {
 			body.accept(visitor);
 		}
-		
+
 		// S05/S09: End function/procedure scope
 		visitor.visit(this);
 	}
