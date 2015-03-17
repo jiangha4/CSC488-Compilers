5d582f9cd13adb0068f32f6481de3ea256ff3b84
Timo Vink <timovink@gmail.com>
2015-03-17 13:05:34 -0400

    Update who-did-what.

--- csc488h.A4.04/RUNALLTESTS.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-#  Location of directory containing  dist/compiler488.jar
-WHERE=.
-#  Compiler reads one source file from command line argument
-#  Output to standard output 
-python3 ./testrunner.py $WHERE/dist/compiler488.jar
-exit 0
