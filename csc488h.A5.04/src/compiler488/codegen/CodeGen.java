3312dbb551230d9ebf43dc583effc02c765b0d03
Timo Vink <timovink@gmail.com>
2015-03-17 19:04:35 -0400

    Clean up CodeGenerator starter code.

--- csc488h.A5.04/src/compiler488/codegen/CodeGen.java
+++ csc488h.A5.04/src/compiler488/codegen/CodeGen.java
@@ -2,12 +2,11 @@ package compiler488.codegen;
 
 import java.io.*;
 import java.util.*;
-import compiler488.compiler.Main;
-import compiler488.runtime.Machine;
-import compiler488.runtime.MemoryAddressException;
-import compiler488.runtime.ExecutionException ;
+import compiler488.ast.*;
+import compiler488.compiler.*;
+import compiler488.runtime.*;
 
-/**      CodeGenerator.java 
+/**	  CodeGenerator.java
  *<pre>
  *  Code Generation Conventions
  *
@@ -18,15 +17,15 @@ import compiler488.runtime.ExecutionException ;
  *  It is assumed that the code generator places instructions
  *  in memory in locations
  *
- *      memory[ 0 .. startMSP - 1 ]
+ *	  memory[ 0 .. startMSP - 1 ]
  *
  *  The code generator may also place instructions and/or
  *  constants in high memory at locations (though this may
  *  not be necessary)
- *      memory[ startMLP .. Machine.memorySize - 1 ]
+ *	  memory[ startMLP .. Machine.memorySize - 1 ]
  *
  *  During program exection the memory area
- *      memory[ startMSP .. startMLP - 1 ]
+ *	  memory[ startMSP .. startMLP - 1 ]
  *  is used as a dynamic stack for storing activation records
  *  and temporaries used during expression evaluation.
  *  A hardware exception (stack overflow) occurs if the pointer
@@ -34,107 +33,51 @@ import compiler488.runtime.ExecutionException ;
  *
  *  The code generator is responsible for setting the global
  *  variables:
- *      startPC         initial value for program counter
- *      startMSP        initial value for msp
- *      startMLP        initial value for mlp
+ *	  startPC		 initial value for program counter
+ *	  startMSP		initial value for msp
+ *	  startMLP		initial value for mlp
  * </pre>
- * @author  <B> PUT YOUR NAMES HERE </B>
+ * @author <B>
+ *	  Haohan Jiang (g3jiangh)
+ *	  Maria Yancheva (c2yanche)
+ *	  Timo Vink (c4vinkti)
+ *	  Chandeep Singh (g2singh)
+ * </B>
  */
+public class CodeGen extends BaseASTVisitor
+{
 
-public class CodeGen
-    {
+	/** initial value for memory stack pointer */
+	private short startMSP;
+	/** initial value for program counter */
+	private short startPC;
+	/** initial value for memory limit pointer */
+	private short startMLP;
 
-    /** initial value for memory stack pointer */
-    private short startMSP;
-    /** initial value for program counter */
-    private short startPC;
-    /** initial value for memory limit pointer */
-    private short startMLP;
+	/** flag for tracing code generation */
+	private boolean traceCodeGen = Main.traceCodeGen ;
 
-    /** flag for tracing code generation */
-    private boolean traceCodeGen = Main.traceCodeGen ;
-
-    /**  
-     *  Constructor to initialize code generation
-     */
-    public CodeGen()
+	/**
+	 * Constructor to initialize code generation
+	 */
+	public CodeGen()
 	{
-	// YOUR CONSTRUCTOR GOES HERE.
+
 	}
 
-    // Utility procedures used for code generation GO HERE.
-
-    /** 
-     *  Additional intialization for gode generation.
-     *  Called once at the start of code generation.
-     *  May be unnecesary if constructor does everything.
-     */
-
-   /** Additional initialization for Code Generation (if required) */
-   void Initialize()
+	/**
+	 *  Write generated code to machine memory and set the PC, MSP, and MLP.
+	 *
+	 *  @throws MemoryAddressException  from Machine.writeMemory
+	 *  @throws ExecutionException	  from Machine.writeMemory
+	 */
+	void Finalize()
+		throws MemoryAddressException, ExecutionException
 	{
-	/********************************************************/
-	/* Initialization code for the code generator GOES HERE */
-	/* This procedure is called once before codeGeneration  */      
-	/*                                                      */
-	/********************************************************/
+		Machine.writeMemory((short)0, Machine.HALT);
 
-	return;
+        Machine.setPC((short)0);
+        Machine.setMSP((short)1);
+        Machine.setMLP((short)(Machine.memorySize - 1));
 	}
-
-    
-    /**  
-     *  Perform any requred cleanup at the end of code generation.
-     *  Called once at the end of code generation.
-     *  @throws MemoryAddressException  from Machine.writeMemory
-     *  @throws ExecutionException      from Machine.writeMemory
-     */
-    void Finalize()
-        throws MemoryAddressException, ExecutionException     // from Machine.writeMemory 
-	{
-	/********************************************************/
-	/* Finalization code for the code generator GOES HERE.  */      
-	/*                                                      */
-	/* This procedure is called once at the end of code     */
-	/* generation                                           */
-	/********************************************************/
-
-	//  REPLACE THIS CODE WITH YOUR OWN CODE
-	//  THIS CODE generates a single HALT instruction 
-        //  as an example.
- 	Machine.setPC( (short) 0 ) ;		/* where code to be executed begins */
-	Machine.setMSP((short)  1 );   	/* where memory stack begins */
-	Machine.setMLP((short) ( Machine.memorySize -1 ) );	
-					/* limit of stack */
-        Machine.writeMemory((short)  0 , Machine.HALT );
-
-	return;
-	}
-
-    /** Procedure to implement code generation based on code generation
-     *  action number
-     * @param actionNumber  code generation action to perform
-     */
-    void generateCode( int actionNumber )
-	{
-	if( traceCodeGen )
-	    {
-		//output the standard trace stream
-		Main.traceStream.println("CodeGen: C" +  actionNumber );
-	    }
-
-	/****************************************************************/
-	/*  Code to implement the code generation actions GOES HERE     */
-	/*  This dummy code generator just prints the actionNumber      */
-	/*  In Assignment 5, you'll implement something more interesting */
-        /*                                                               */
-        /*  FEEL FREE TO ignore or replace this procedure                */
-	/****************************************************************/
-
-        System.out.println("Codegen: C" + actionNumber ); 
-	return;
-	}
-
-     //  ADDITIONAL FUNCTIONS TO IMPLEMENT CODE GENERATION GO HERE
-
 }
